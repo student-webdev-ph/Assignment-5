679b06d554c775e328ac78eb408c254c36568500
student-webdev-ph <mdaltafuddin456@gmail.com>
2025-03-03 20:02:51 +0600

    90% complete

--- script/ques.js
+++ script/ques.js
@@ -23,13 +23,34 @@ if (deskBtn){
     })
 }
 
+const img = document.getElementById("colors");
+const colors = ["#ff0000", "#ff7f00", "#ffff00", "#00ff00", "#0000ff", "#4b0082", "#8b00ff"];
+img.addEventListener('click', function(){
+    const randomColors = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColors;
+})
+
+
+
+// const img = document.getElementById("colors").addEventListener('click', function () {
+//     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+//     document.body.style.backgroundColor = randomColor;
+// })
+
+
 
       //btn1
-const btn1 = document.getElementById('btn1').addEventListener('click', function(){
+    const btn1 = document.getElementById('btn1').addEventListener('click', function(){
     const task = document.getElementById('task').innerText;
     const convertedTask = parseInt(task);
     const totalTask =  document.getElementById('total-task').innerText;
     const convertedTotalTask = parseInt (totalTask);
+    const title1 = document.getElementById('title-1');
+    const titleText1 = title1.innerText;
+    const commentContainer = document.getElementById('comment-container');
+    commentContainer.innerText = 'You have Complete The Task '  + title1.innerText +  ' at 12:48:15 PM';
+    commentContainer.append("")
+    
 
     if(convertedTask > 0) {
         const subs = convertedTask - 1;
@@ -39,6 +60,10 @@ const btn1 = document.getElementById('btn1').addEventListener('click', function(
     }
     this.disabled = true;
     this.style.backgroundColor ='#a9a9a9';
+
+    if (parseInt(task.innerText) === 0) {
+        alert("this is 0");
+    }
 })
 
 
@@ -48,6 +73,11 @@ const btn2 = document.getElementById('btn2').addEventListener('click', function(
     const convertedTask = parseInt(task);
     const totalTask =  document.getElementById('total-task').innerText;
     const convertedTotalTask = parseInt (totalTask);
+    const title2 = document.getElementById('title-2');
+    const titleText2 = title2.innerText;
+    const commentContainer = document.getElementById('comment-container');
+    commentContainer.innerText = 'You have Complete The Task '  + title2.innerText +  ' at 12:48:15 PM';
+    commentContainer.append("")
 
     if(convertedTask > 0) {
         const subs = convertedTask - 1;
@@ -57,6 +87,10 @@ const btn2 = document.getElementById('btn2').addEventListener('click', function(
     }
     this.disabled = true;
     this.style.backgroundColor ='#a9a9a9';
+
+    if (parseInt(task.innerText) === 0){
+        alert("this is 0");
+    }
 })
 
 
@@ -66,6 +100,11 @@ const btn3 = document.getElementById('btn3').addEventListener('click', function(
     const convertedTask = parseInt(task);
     const totalTask =  document.getElementById('total-task').innerText;
     const convertedTotalTask = parseInt (totalTask);
+    const title3 = document.getElementById('title-3');
+    const titleText3 = title3.innerText;
+    const commentContainer = document.getElementById('comment-container');
+    commentContainer.innerText = 'You have Complete The Task '  + title3.innerText +  ' at 12:48:15 PM';
+    commentContainer.append("")
 
     if(convertedTask > 0) {
         const subs = convertedTask - 1;
@@ -75,6 +114,10 @@ const btn3 = document.getElementById('btn3').addEventListener('click', function(
     }
     this.disabled = true;
     this.style.backgroundColor ='#a9a9a9';
+
+    if (parseInt(task.innerText) === 0){
+        alert("this is 0");
+    }
 })
 
 
@@ -84,6 +127,12 @@ const btn3 = document.getElementById('btn3').addEventListener('click', function(
     const convertedTask = parseInt(task);
     const totalTask =  document.getElementById('total-task').innerText;
     const convertedTotalTask = parseInt (totalTask);
+    const title4 = document.getElementById('title-4');
+    const titleText4 = title4.innerText;
+    const commentContainer = document.getElementById('comment-container');
+    commentContainer.innerText = 'You have Complete The Task '  + title4.innerText +  ' at 12:48:15 PM';
+    commentContainer.append()
+
 
     if(convertedTask > 0) {
         const subs = convertedTask - 1;
@@ -93,6 +142,10 @@ const btn3 = document.getElementById('btn3').addEventListener('click', function(
     }
     this.disabled = true;
     this.style.backgroundColor ='#a9a9a9';
+
+    if (parseInt(task.innerText) === 0){
+        alert("this is 0");
+    }
 })
 
 
@@ -102,6 +155,11 @@ const btn3 = document.getElementById('btn3').addEventListener('click', function(
     const convertedTask = parseInt(task);
     const totalTask =  document.getElementById('total-task').innerText;
     const convertedTotalTask = parseInt (totalTask);
+    const title5 = document.getElementById('title-5');
+    const titleText5 = title5.innerText;
+    const commentContainer = document.getElementById('comment-container');
+    commentContainer.innerText = 'You have Complete The Task '  + title5.innerText +  ' at 12:48:15 PM';
+    commentContainer.append("");
 
     if(convertedTask > 0) {
         const subs = convertedTask - 1;
@@ -111,6 +169,10 @@ const btn3 = document.getElementById('btn3').addEventListener('click', function(
     }
     this.disabled = true;
     this.style.backgroundColor ='#a9a9a9';
+
+    if (parseInt(task.innerText) === 0){
+        alert("this is 0");
+    }
 })
 
 
@@ -120,6 +182,11 @@ const btn3 = document.getElementById('btn3').addEventListener('click', function(
     const convertedTask = parseInt(task);
     const totalTask =  document.getElementById('total-task').innerText;
     const convertedTotalTask = parseInt (totalTask);
+    const title6 = document.getElementById('title-6');
+    const titleText6 = title6.innerText;
+    const commentContainer = document.getElementById('comment-container');
+    commentContainer.innerText = 'You have Complete The Task '  + title6.innerText +  ' at 12:48:15 PM';
+    commentContainer.append();
 
     if(convertedTask > 0) {
         const subs = convertedTask - 1;
@@ -129,5 +196,20 @@ const btn3 = document.getElementById('btn3').addEventListener('click', function(
     }
     this.disabled = true;
     this.style.backgroundColor ='#a9a9a9';
+
+    if (parseInt(task.innerText) === 0){
+        alert("this is 0");
+    }
 })
+
+const history = document.getElementById('history').addEventListener('click', function(){
+    document.getElementById('comment-container').innerText = ''
+})
+
+
+const alert = document.getElementById('task').innerText
+convertedAlert = parseInt(alert)
+if(convertedAlert === 0 ){
+    alert();
+}
 
